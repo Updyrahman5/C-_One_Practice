2b2e87dc426da7bc4bb5b37dbf13189c143d06d2
Updyrahman5 <halaagboys10@gmail.com>
2026-09-25 10:21:14 +0300

    Assignment1

---
+++ Assignment1/Assignment1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dweek = new System.Windows.Forms.TextBox();
+            this.year = new System.Windows.Forms.TextBox();
+            this.month = new System.Windows.Forms.TextBox();
+            this.dMonth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dweek
+            // 
+            this.dweek.Location = new System.Drawing.Point(237, 32);
+            this.dweek.Name = "dweek";
+            this.dweek.Size = new System.Drawing.Size(165, 20);
+            this.dweek.TabIndex = 0;
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(237, 110);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(165, 20);
+            this.year.TabIndex = 1;
+            // 
+            // month
+            // 
+            this.month.Location = new System.Drawing.Point(237, 58);
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(165, 20);
+            this.month.TabIndex = 2;
+            // 
+            // dMonth
+            // 
+            this.dMonth.Location = new System.Drawing.Point(237, 84);
+            this.dMonth.Name = "dMonth";
+            this.dMonth.Size = new System.Drawing.Size(165, 20);
+            this.dMonth.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Day Of Month";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter the month";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter the day of week";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter the year";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Show data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Result
+            // 
+            this.Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result.Location = new System.Drawing.Point(138, 146);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(330, 23);
+            this.Result.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 36);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dMonth);
+            this.Controls.Add(this.month);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.dweek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dweek;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.TextBox month;
+        private System.Windows.Forms.TextBox dMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Assignment1/Assignment1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dayweek = dweek.Text;
+            string mon = month.Text;
+            string daymonth = dMonth.Text;
+            string yr = year.Text;
+
+            string result = dayweek + " " + mon + " " + daymonth + " " + yr;
+            Result.Text = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dweek.Clear();
+            month.Clear();
+            dMonth.Clear();
+            year.Clear();
+            Result.Text=" ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
